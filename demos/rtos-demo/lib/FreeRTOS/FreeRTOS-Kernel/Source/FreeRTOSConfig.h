d160f3e78ba91dc4c16ca8c90485e7a05eb478bd
Josh Brake <joshbrake@users.noreply.github.com>
2023-11-30 13:58:21 -0800

    Fixed errors related to missing config options.

--- demos/rtos-demo/lib/FreeRTOS/FreeRTOS-Kernel/Source/FreeRTOSConfig.h
+++ demos/rtos-demo/lib/FreeRTOS/FreeRTOS-Kernel/Source/FreeRTOSConfig.h
@@ -48,8 +48,8 @@
 #endif
 
 #define configUSE_PREEMPTION			1
-#define configUSE_IDLE_HOOK				1
-#define configUSE_TICK_HOOK				1
+#define configUSE_IDLE_HOOK				0
+#define configUSE_TICK_HOOK				0
 #define configCPU_CLOCK_HZ				( SystemCoreClock )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 5 )
@@ -61,9 +61,9 @@
 #define configIDLE_SHOULD_YIELD			1
 #define configUSE_MUTEXES				1
 #define configQUEUE_REGISTRY_SIZE		8
-#define configCHECK_FOR_STACK_OVERFLOW	2
+#define configCHECK_FOR_STACK_OVERFLOW	0
 #define configUSE_RECURSIVE_MUTEXES		1
-#define configUSE_MALLOC_FAILED_HOOK	1
+#define configUSE_MALLOC_FAILED_HOOK	0
 #define configUSE_APPLICATION_TASK_TAG	0
 #define configUSE_COUNTING_SEMAPHORES	1
 #define configGENERATE_RUN_TIME_STATS	0
