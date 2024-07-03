7870319cb80075a94293f5d2bfb4eddc5421800d
Josh Brake <joshbrake@users.noreply.github.com>
2024-07-02 22:16:46 -0700

    Remove web subfolder from public E155 repo

--- web/upload_to_web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "/Users/joshbrake/dev/E155/"
-quarto render web/
-
-rsync --update -rv web/_site/ "/Users/joshbrake/Library/CloudStorage/GoogleDrive-jbrake@g.hmc.edu/Shared drives/wwwbrake/class/e155/fa23"
-
-sleep 10
