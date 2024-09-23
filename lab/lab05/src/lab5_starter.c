05816c9df9325f3f4e67edc8652176c6e1fda9ff
Josh Brake <joshbrake@users.noreply.github.com>
2024-09-23 10:26:07 -0700

    Revert "Updated Lab 4 (#5)"
    This reverts commit c5787f6b9614f6f07571d5bf1df980f82c137749.

--- lab/lab05/src/lab5_starter.c
+++ lab/lab05/src/lab5_starter.c
@@ -1,6 +1,6 @@
-// lab4_starter.c
-// Fur Elise, E155 Lab 4
-// Updated Fall 2024
+// lab5_starter.c
+// Fur Elise, E155 Lab 5
+// Updated Fall 2021
 
 // Pitch in Hz, duration in ms
 const int notes[][2] = {
